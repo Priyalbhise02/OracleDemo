5107920cd19cdce36ed25d24b6502f3beed00523
Priyalbhise02 <priyal.bhise@flexagon.com>
2026-03-20 17:01:52 +0530

    Update DeleteOperation.sql

--- fdtlt89/SQL/DeleteOperation.sql
+++ fdtlt89/SQL/DeleteOperation.sql
@@ -1,1 +1,11 @@
+-- Delete specific employee
+DELETE FROM Employee
+WHERE emp_id = 101;
 
+-- Delete employees from HR department
+DELETE FROM Employee
+WHERE department = 'HR';
+
+-- Delete employees with low salary
+DELETE FROM Employee
+WHERE salary < 20000;
